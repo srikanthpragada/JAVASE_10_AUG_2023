014f015536a5948a3bbf924a33bfe7cb47680c54
srikanthpragada <srikanthpragada@gmail.com>
2023-08-18 17:56:13 +0530

    18-AUG

---
+++ demo/src/oop1/Account.java
@@ -0,0 +1,22 @@
+package oop1;
+
+public class Account {
+	// Instance variables
+	private int acno;
+	private String ahname;
+	private double balance;
+
+	// Methods
+	public void open(int no, String name) {
+		acno = no;
+		ahname = name;
+		balance = 0;
+	}
+
+	public void print() {
+		System.out.println(acno);
+		System.out.println(ahname);
+		System.out.println(balance);
+	}
+
+}
---
+++ demo/src/oop1/TestAccount.java
@@ -0,0 +1,14 @@
+package oop1;
+
+public class TestAccount {
+
+	public static void main(String[] args) {
+		Account a1;  // Object reference
+		
+		a1 = new Account(); // Object 
+		a1.open(1,  "James Gosling");
+		a1.print();
+        
+	}
+
+}
