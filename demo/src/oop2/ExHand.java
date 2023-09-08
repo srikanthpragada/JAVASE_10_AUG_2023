55ffc617a9878e281aff9616dbbf99670d2719b8
srikanthpragada <srikanthpragada@gmail.com>
2023-09-08 18:58:02 +0530

    08-SEP

--- demo/src/oop2/ExHand.java
+++ demo/src/oop2/ExHand.java
@@ -3,15 +3,17 @@ package oop2;
 public class ExHand {
 
 	public static void main(String[] args) {
-		String s = "1A";
+		String s = "0";
 
 		try {
 			int a = Integer.parseInt(s);
-			System.out.println(a);
+			System.out.println(100 / a);
 		} catch (NumberFormatException ex) {
-            System.out.println("Invalid Number!");
+			System.out.println("Invalid Number!");
+		} catch (ArithmeticException ex) {
+			System.out.println(ex.getMessage());
 		}
-		
+
 		System.out.println("The End");
 
 	}
